549c731277d542baddcd68243c5be4f5fc8d5625
aaaanilkumar <aaaanilkumar@gmail.com>
2019-01-15 22:47:40 +1100

    inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hello World</h1>
+       <Header/ >
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from "@material-ui/core/AppBar";
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
+
+const Header = props => {
+    const { classes } = props;
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="Menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+          Pet Shop
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default withStyles(styles)(Header);
